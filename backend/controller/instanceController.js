157e3254c53a89cf69b539e3317ac49d1e56d2ef
kush987 <kushal9870@gmail.com>
2023-11-10 23:10:18 +0530

    Docker engine api implemented

--- backend/controller/instanceController.js
+++ backend/controller/instanceController.js
@@ -4,140 +4,199 @@ const User = require('../models/User');
 const UserInstance = require('../models/UsersInstance');
 const { execSync } = require('child_process');
 const WorkSpace = require('../models/workspaceModel');
-
+const axios = require('axios');
 class InstanceController {
 
-    createInstance = async (req, res, next) => {
 
+    createInstance = async (req, res, next) => {
         let instance_name = req.body.instance;
         let resultInstance = await Product.findOne({ 'instance_name': instance_name });
-        // let workSpace = await WorkSpace.findOne({'user_id':req.body.user_id})
         let projectId = req.body.projectId;
         const username = resultInstance.toJSON().instance_name + '-' + req.body.username;
         let container_id = '';
         let instance_Name = '';
-        if(!projectId){
-            res.send({'message':"please select your project workspace"})
-        }else{
-
-            try {
-                // const result = execSync('docker-compose -f /media/diatoz/New Volume/testAngular/ticketzapp/e-commerce-store/blender-compose.yml up -d');
-                if (instance_name ==='blender'){
     
-                    const result = execSync(`docker run -d \
-                   --name=${username} --network=my_custom_network \
-                   --security-opt seccomp=unconfined \
-                   -e PUID=1000 \
-                   -e PGID=1000 \
-                   -e TZ=Etc/UTC \
-                   -e SUBFOLDER=/ \
-                   -p 3000 \
-                   -v /path/to/config:/config \
-                   --restart unless-stopped \
-                   -P \
-                   ${resultInstance.toJSON().image}`)
-                }else if (instance_name ==='vscode'){
-                    const result = execSync(`docker run -d \
-                    --name=${username} \
-                    -e PUID=1000 \
-                    -e PGID=1000 \
-                    -e TZ=Etc/UTC \
-                    -e PASSWORD=123456 \
-                    -e HASHED_PASSWORD= \
-                    -e SUDO_PASSWORD=password \
-                    -e SUDO_PASSWORD_HASH= \
-                    -e PROXY_DOMAIN=code-server.my.domain \
-                    -e DEFAULT_WORKSPACE=/config/workspace \
-                    -p 8443 \
-                    -v /path/to/appdata/config:/config \
-                    --restart unless-stopped \
-                    ${resultInstance.toJSON().image}
-                  `)
-                }
-            } catch (error) {
-                console.error(`Error executing the command: ${error}`);
-            }
-            try {
-                const result = execSync(`docker ps -qf "name=${username}"`)
-                container_id = result.toString().replace(/\s+/g, '');
-                instance_Name = resultInstance.toJSON().instance_name;
-                await UserInstance.create({ 'user_id': req.body.user_id, 'projectId':projectId, 'container_id': container_id, 'instance_name': resultInstance.toJSON().instance_name,'status':'running' });
+        if (!projectId) {
+            res.send({ 'message': "please select your project workspace" });
+            return;
+        }
     
-            } catch (error) {
-                res.send("error")
+        try {
+            let dockerApiEndpoint = 'http://localhost:2375'; // Replace with your Docker API endpoint
+            if (instance_name === 'blender') {
+                const response = await axios.post(`${dockerApiEndpoint}/containers/create`, {
+                    name: username,
+                    Image: resultInstance.toJSON().image,
+                    HostConfig: {
+                        NetworkMode: 'my_custom_network',
+                        SecurityOpt: ['seccomp=unconfined'],
+                        PortBindings: { '3000/tcp': [{}] },
+                        RestartPolicy: { Name: 'unless-stopped' },
+                        Binds: ['/path/to/config:/config']
+                    }
+                });
+                container_id = response.data.Id;
+            } else if (instance_name === 'vscode') {
+                const response = await axios.post(`${dockerApiEndpoint}/containers/create`, {
+                    name: username,
+                    Image: resultInstance.toJSON().image,
+                    HostConfig: {
+                        PortBindings: { '8443/tcp': [{}] },
+                        RestartPolicy: { Name: 'unless-stopped' },
+                        Binds: ['/path/to/appdata/config:/config']
+                    },
+                    Env: [
+                        'PUID=1000',
+                        'PGID=1000',
+                        'TZ=Etc/UTC',
+                        'PASSWORD=',
+                        'SUDO_PASSWORD=password',
+                        'PROXY_DOMAIN=code-server.my.domain',
+                        'DEFAULT_WORKSPACE=/config/workspace'
+                    ]
+                });
+                container_id = response.data.Id;
             }
-            res.send({ 'user_id': req.body.user_id,'projectId':projectId, 'container_id': container_id , 'instance_name': instance_Name ,'status':'running'})
+    
+            // Start the container
+            await axios.post(`${dockerApiEndpoint}/containers/${container_id}/start`);
+    
+            instance_Name = resultInstance.toJSON().instance_name;
+    
+            await UserInstance.create({
+                'user_id': req.body.user_id,
+                'projectId': projectId,
+                'container_id': container_id,
+                'instance_name': resultInstance.toJSON().instance_name,
+                'status': 'running'
+            });
+    
+            res.send({
+                'user_id': req.body.user_id,
+                'projectId': projectId,
+                'container_id': container_id,
+                'instance_name': instance_Name,
+                'status': 'running'
+            });
+        } catch (error) {
+            console.error('Error creating or starting container:', error.message);
+            res.status(500).send({ error: 'Internal Server Error' });
         }
     }
 
-    startInstance = async (req,res,next) =>{
-        let container_id = req.body.container_id;
-        try{
-            const result = await execSync(`docker start ${container_id}`);
-            await UserInstance.findOneAndUpdate({'user_id':req.params.param,'container_id':req.body.container_id},[{$set:{'status':'running'}}])
-        } catch (err){
-            res.send({message:"error"})
+    startInstance = async (req, res, next) => {
+        try {
+            const container_id = req.body.container_id;
+            const dockerApiEndpoint = 'http://localhost:2375'; // Replace with your Docker API endpoint
+    
+            // Send a POST request to start the container
+            await axios.post(`${dockerApiEndpoint}/containers/${container_id}/start`);
+    
+            // Update the status in the database
+            await UserInstance.findOneAndUpdate(
+                { 'user_id': req.params.param, 'container_id': req.body.container_id },
+                { $set: { 'status': 'running' } }
+            );
+    
+            res.send({ message: "Instance is started" });
+        } catch (error) {
+            console.error('Error starting container:', error.message);
+            res.status(500).send({ message: "Error starting instance" });
         }
-        res.send({message:"Instance is start"})
     }
 
-    stopInstance = async (req,res,next) =>{
-       let container_id =  req.body.container_id;
-       try {
-        const result = await execSync(`docker stop ${container_id}`);
-        try{
-
-            await UserInstance.findOneAndUpdate({'user_id':req.params.param,'container_id':req.body.container_id},[{$set:{'status':'stop'}}])
-        }catch(err){
-            res.send({message: err})
+    stopInstance = async (req, res, next) => {
+        try {
+            const containerId = req.body.container_id;
+            const dockerApiEndpoint = 'http://localhost:2375'; // Replace with your Docker API endpoint
+    
+            // Send a POST request to stop the container
+            await axios.post(`${dockerApiEndpoint}/containers/${containerId}/stop`);
+    
+            // Update the status in the database
+            await UserInstance.findOneAndUpdate(
+                { 'user_id': req.params.param, 'container_id': req.body.container_id },
+                { $set: { 'status': 'stopped' } }
+            );
+    
+            res.send({ message: "Instance is stopped", status: "stopped" });
+        } catch (error) {
+            console.error('Error stopping container:', error.message);
+            res.status(500).send({ message: "Error stopping instance" });
         }
-       } catch(err){
-        res.send({message:'error',err})
-       }
-       res.send({message:'Instance is stop', status:"stop"})
     }
 
     removeInstance = async (req, res, next) => {
-        let container_id = req.body.container_id;
         try {
-
-            const result = execSync(`docker stop ${container_id}`)
-            await execSync(`docker rm ${container_id}`)
-            await UserInstance.findOneAndDelete({ container_id: container_id }, (err, deletedDoc) => {
+            const containerId = req.body.container_id;
+            const dockerApiEndpoint = 'http://localhost:2375'; // Replace with your Docker API endpoint
+    
+            // Send a POST request to stop the container
+            const inspectResponse = await axios.get(`${dockerApiEndpoint}/containers/${containerId}/json`);
+            if (inspectResponse.data.State.Status !== 'exited') {
+                // If not already stopped, stop the container
+                await axios.post(`${dockerApiEndpoint}/containers/${containerId}/stop`);
+            }
+    
+            // Send a DELETE request to remove the container
+            await axios.delete(`${dockerApiEndpoint}/containers/${containerId}`);
+    
+            // Update the status in the database
+            await UserInstance.findOneAndDelete({ container_id: containerId }, (err, deletedDoc) => {
                 if (err) {
-                    console.log({message:'Error deleting document:'});
+                    console.error('Error deleting document:', err);
                 } else if (deletedDoc) {
-                    console.log({message:'Deleted document:'});
+                    console.log('Deleted document:', deletedDoc);
                 } else {
-                    console.log({message:'Document not found'});
+                    console.log('Document not found');
                 }
             });
+    
+            res.send({ message: 'Instance is deleted' });
         } catch (error) {
-            res.send({message:'error',error})
+            console.error('Error removing container:', error.message);
+            res.status(500).send({ message: 'Error removing instance' });
         }
-        res.send({message:'Instance is deleted'})
     }
 
-    getInstance = async (req, res, next) =>{
-        let containerID = await UserInstance.find({user_id: req.params.param},( err, documents)=>{
-            if(err){
-                res.send({message:'Error retrieving documents: ',err});
+    getInstance = async (req, res, next) => {
+        try {
+            const userId = req.params.param;
+            const dockerApiEndpoint = 'http://localhost:2375'; // Replace with your Docker API endpoint
+    
+            // Retrieve containers for the user
+            const userContainers = await UserInstance.find({ user_id: userId });
+    
+            // Extract information for each container
+            const extractedValues = [];
+            for (const container of userContainers) {
+                const containerId = container.container_id;
+                
+                // Retrieve information about the container from the Docker API
+                const inspectResponse = await axios.get(`${dockerApiEndpoint}/containers/${containerId}/json`);
+                
+                if (inspectResponse.data.State.Running === true) {
+                    const portMappings = inspectResponse.data.NetworkSettings.Ports;
+                    const key = Object.keys(portMappings)[0]; // Assuming there's only one port mapping
+                    
+                    const hostPort = portMappings[key][0].HostPort.split('/');
+                    const hostIp = portMappings[key][0].HostIp.split('/');
+                    
+                    const ipAddress = hostPort[0] ? `${hostIp[0]}:${hostPort[0]}` : '';
+                    
+                    extractedValues.push({ 'container_id': container, 'ip_address': ipAddress });
+                } else {
+                    extractedValues.push({ 'container_id': container, 'ip_address': '' });
+                }
             }
-                // res.send('All documents in the collection: ', documents);
-        });
-        let extractedValues = [];
-        for(let i= 0; i<containerID.length;i++){
-            const result = await execSync(`docker port ${containerID[i].toJSON().container_id.replace(/\s+/g, '')}`);
-            const line = result.toString().split('\n')[0];
-            const match = line.match(/\d+\.\d+\.\d+\.\d+:\d+/);
-            if(match){
-                extractedValues.push({'container_id':containerID[i],'ip_addresss':match[0]})
-            }else{
-                extractedValues.push({'container_id':containerID[i],'ip_addresss':''})
-            }
+
+            res.send({ 'data': extractedValues });
+        } catch (error) {
+            console.error('Error retrieving container information:', error.message);
+            res.status(500).send({ message: 'Error retrieving container information' });
         }
-        res.send({'data':extractedValues});
     }
+    
 }
 
 module.exports = new InstanceController;
